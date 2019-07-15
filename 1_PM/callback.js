4ffe138c7ec84299d3900ff4352294d422e44c12
Spencer Tiberi <spencertiberi@gmail.com>
2019-07-15 16:45:00 -0400

    Add more structure to callback.js

--- 1_PM/callback.js
+++ 1_PM/callback.js
@@ -3,16 +3,22 @@
 // flips a coin and tells the user whether or not their guess was correct
 
 const coinToss = (guess, callback) => {
-  const result = Math.round(Math.random());
-  const didWin = guess === result;
-  callback(didWin);
-};
+  const result = Math.round(Math.random())
+  const didWin = guess === result
+  callback(didWin)
+}
 
 const getUser = callback => {
   const user = {
     firstName: "John",
     lastName: "Doe",
     guess: Math.round(Math.random())
-  };
-  callback(user);
-};
+  }
+  // TODO: Run the callback function here with the appropriate parameters
+}
+
+const handleWin = didWin => {
+  // TODO
+}
+
+// TODO: run getUser
