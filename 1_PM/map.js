8567f6e957dc395c1da86cdac18cdec9a0d4d9f4
Spencer Tiberi <spencertiberi@gmail.com>
2019-07-15 16:11:12 -0400

    Make map implementation a challenge

--- 1_PM/map.js
+++ 1_PM/map.js
@@ -1,14 +1,18 @@
 // Complete the implementation of the following functions so that
 // the output is [0, 3, 6, 9, 12]
 
-const arr = [0, 1, 2, 3, 4];
+const arr = [0, 1, 2, 3, 4]
 
-const triple = () => {
-  // TODO
-};
+const triple = el => {
+  // TODO: Triple the value inputted
+}
 
-Array.prototype.map = function(fun) {
-  // TODO
-};
+// Challenge: Implement the map method below!
+// Array.prototype.map = function(fun) {
+//   // this contains the value of the Array
+//   const arr = this
+//   // TODO: Using the passed in fun, apply to all elements
+//   // to array and return the new array
+// }
 
-console.log(arr.map(triple));
+console.log(arr.map(triple))
