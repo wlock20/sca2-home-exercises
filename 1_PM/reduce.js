42fac0ef4695905e0a9c70041a1169ce075722ae
Spencer Tiberi <spencertiberi@gmail.com>
2019-07-15 16:42:55 -0400

    Make reduce implementation a challenge

--- 1_PM/reduce.js
+++ 1_PM/reduce.js
@@ -2,11 +2,18 @@
 // returns the product of its values
 // [0,4,3,6,9].fold(sum, 0) === 22
 
-const arr = [0, 1, 2, 3, 4];
+const arr = [0, 1, 2, 3, 4]
 
-const sum = (acc, cur) => {};
+// Note: acc stands for accumulator, cur stands for current
+const sum = (acc, cur) => {}
 
-Array.prototype.reduce = function(fun, init) {};
+// // Challenge: Implement the reduce method below!
+// Array.prototype.reduce = function(fun, initValue) {
+//   // this contains the value of the Array
+//   const arr = this
+//   // TODO: Using the passed in fun, apply to all elements
+//   // to array and return a singular value
+// }
 
-const total = arr.reduce(sum, 0);
-console.log(total);
+const total = arr.reduce(sum, 0)
+console.log(total)
