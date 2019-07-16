439357cee9b5c8d4aad33e385407c001d72b8abe
Spencer Tiberi <spencertiberi@gmail.com>
2019-07-16 15:20:21 -0400

    Update 2_PM file structure

--- 2_PM/index.js
+++ 2_PM/index.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const path = require("path")
+const app = express()
 
-app.use(express.static(__dirname + "/View"));
-app.use(express.static(__dirname + "/Script"));
+app.use(express.static(__dirname + "/public"))
+const views = path.join(__dirname, "views")
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
-});
+  res.sendFile("index.html", { root: views })
+})
 
-app.listen(3000);
-console.log("Running at port 3000");
+app.listen(3000)
+console.log("Running at port 3000")
